3935835a06719d2e24d6b496ade9e603bfc2d427
Luis <luisfmir@gmail.com>
2020-06-28 13:53:28 +0100

    Update script

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function openLink() {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
     if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) {
-        window.location.href = 'applinks://';
+        window.location.href = 'applinks:';
 
         setTimeout(
             () => { window.location = 'https://paymoo.github.io';},
-            1000
+            2000
         );
     } else {
         window.location = 'https://www.google.com'
